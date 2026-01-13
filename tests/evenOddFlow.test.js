96d347a0489bdae54da9e942937ea0502362decb
sofiaas16 <sarahzambrano2019@gmail.com>
2026-01-13 16:35:41 -0500

    feat: :sparkles:

--- tests/evenOddFlow.test.js
+++ tests/evenOddFlow.test.js
@@ -1,21 +1,37 @@
+import { buildEvenOddFlow } from "../src/evenOddFlow";
 
 describe("EvenOddFlow (PocketFlow)", () =>{
     test("Número par", async ()=>{
-        const flow = buildEvenOddFlow(); //Creación del flujo
-        const shared = {number: 4};
-        
+        const flow = buildEvenOddFlow(); // Creación del flujo
+        const shared =  {number: 4};
+
         await flow.run(shared);
 
-        expect(shared.result).toBe("even)");
-
+        expect(shared.result).toBe("even");
     });
-    
-    test("Número impar", async ()=>{
-        const flow =buildEvenOddFlow();
-        const shared = {number: 9};
 
-        await flow.run (shared);
+    test("Número par", async ()=>{
+        const flow = buildEvenOddFlow(); // Creación del flujo
+        const shared =  {number: 9};
+
+        await flow.run(shared);
 
         expect(shared.result).toBe("odd");
-    })
+    });
+
+    test("Número par", async ()=>{
+        const flow = buildEvenOddFlow(); // Creación del flujo
+        const shared =  {number: 0};
+
+        await flow.run(shared);
+
+        expect(shared.result).toBe("even");
+    });
 })
+
+/*
+ ### RESUMEN ###
+ 1. Cada "test()" es un caso de estudio/prueba.
+ 2. "par", "impar", "cero" son entradas válidas.
+ 3. "string" será un caso inválido, por lo cual se esperará un error (caso negativo).
+*/
